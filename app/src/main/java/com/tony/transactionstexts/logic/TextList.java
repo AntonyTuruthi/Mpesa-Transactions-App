0ee82e6f14d81c021069d6641f31b6eb5a3237e5
@turuthi <antonyturuthi>
2022-01-04 11:41:04 +0300

    Added the TextList.java which takes in the texts as they are being read directly from the inbox

---
+++ app/src/main/java/com/tony/transactionstexts/logic/TextList.java
@@ -0,0 +1,15 @@
+package com.tony.transactionstexts.logic;
+
+public class TextList {
+
+    String transactionText;
+
+    public TextList(String transactionText){
+            this.transactionText = transactionText;
+    }
+
+    public String getTransactionText() {
+            return transactionText;
+    }
+
+}
